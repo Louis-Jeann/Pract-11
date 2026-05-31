0c9c242f486b7fb0d53765295419bebe62c0f202
Louis J <132916937+Louis-Jeann@users.noreply.github.com>
2026-05-30 21:31:37 -0400

    Practica #1
    eso eso eso

---
+++ #1/#1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nombre;
+        string apellido;
+        int edad;
+        double altura;
+
+        Console.Write("Ingrese su nombre: ");
+        nombre = Console.ReadLine();
+
+        Console.Write("Ingrese su apellido: ");
+        apellido = Console.ReadLine();
+
+        Console.Write("Ingrese su edad: ");
+        edad = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese su altura: ");
+        altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n--- Información ingresada ---");
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Apellido: " + apellido);
+        Console.WriteLine("Edad: " + edad);
+        Console.WriteLine("Altura: " + altura);
+
+        Console.WriteLine("\nHola, mi nombre es " + nombre + " " + apellido +
+                          " y tengo " + edad + " años. Mi altura es " + altura + " metros.");
+    }
+}
